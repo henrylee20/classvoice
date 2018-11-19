f43e61de117864aff875c0dce29ff65e67f92c89
HenryLee20 <henrylee6991@gmail.com>
2018-11-19 15:46:51 +0800

    add a test

---
+++ src/test/java/me/henrylee/classvoice/controller/DemoControllerTest.java
@@ -0,0 +1,23 @@
+package me.henrylee.classvoice.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class DemoControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void getDemoEntity() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.post("/demo"));
+    }
+}
